f658cb629139a32304b1a0498fada285fab87372
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-11-11 15:31:51 -0500

    starting the flexbox party

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -5,7 +5,16 @@ export default Route.extend({
     return [{
       dateRecorded: '12-12-2017',
       timeOfDay: 'Afternoon',
-      content: 'Grand Old Mansion',
+      content: 'So So Dank',
+    },{
+      dateRecorded: '12-15-2017',
+      timeOfDay: 'Morning',
+      content: 'Something was really sweet',
+    },
+    {
+      dateRecorded: '12-16-2017',
+      timeOfDay: 'Evening',
+      content: 'Something was really bitter',
     }];
   }
 });
