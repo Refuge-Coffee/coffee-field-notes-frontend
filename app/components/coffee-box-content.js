0d2ddedc397286371e315ade54b4017629b9229f
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-01-13 22:46:15 -0500

    configuring modal addon; adding in example

--- app/components/coffee-box-content.js
+++ app/components/coffee-box-content.js
@@ -2,10 +2,16 @@ import Component from '@ember/component';
 
 export default Component.extend({
   coffee: null,
+  isShowingModal: false,
 
   actions: {
     displayCoffee() {
+      this.toggleProperty('isShowingModal');
       console.log(this.get('coffee'));
+    },
+
+    toggleModal() {
+      this.toggleProperty('isShowingModal');
     }
   }
 });
