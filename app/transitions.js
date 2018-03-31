4c8711ededf42e3353c3427c50383ab9952484a8
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-03-31 14:22:45 -0400

    got first animation working:

--- app/transitions.js
+++ app/transitions.js
@@ -1,9 +1,9 @@
 export default function(){
   // Add your transitions here, like:
-  //   this.transition(
-  //     this.fromRoute('people.index'),
-  //     this.toRoute('people.detail'),
-  //     this.use('toLeft'),
-  //     this.reverse('toRight')
-  //   );
+  this.transition(
+    this.fromRoute('notes'),
+    this.toRoute('beans'),
+    this.use('toRight'),
+    this.reverse('toLeft')
+  );
 }
