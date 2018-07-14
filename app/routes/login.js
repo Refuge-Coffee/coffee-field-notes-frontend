e5ffda32e6b53167b3008205775af40118179514
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-07-14 19:06:31 -0400

    more auth work

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,19 +1,21 @@
 import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
-import config from '../config/environment';
 
 export default Route.extend( {
   
   session: service('session'),
+  email: "",
+  password: "",
 
   actions: {
     authenticateWithOAuth2() {
-      let { identification, password } = this.getProperties('identification', 'password');
-      this.get('session').authenticate('authenticator:oauth2', identification, password)
+      let { email, password } = this.getProperties('email', 'password');
+      this.get('session').authenticate('authenticator:oauth2', email, password)
         .then(() => {
           this.get('rememberMe') && this.set('session.store.cookieExpirationTime', 60 * 60 * 24 * 14);
         })
         .catch((reason) => {
+          debugger;
           this.set('errorMessage', reason.error);
         });
     }
