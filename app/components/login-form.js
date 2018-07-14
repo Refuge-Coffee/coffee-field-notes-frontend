7651f6414c92b9cf8af6aaca21867cdb3549bdd7
hiimtaylorjones <taylorjonessoftware@gmail.com>
2018-07-14 19:16:58 -0400

    moving login to component

---
+++ app/components/login-form.js
@@ -0,0 +1,20 @@
+import { inject as service } from '@ember/service';
+import Component from '@ember/component';
+import config from '../config/environment';
+
+export default Component.extend({
+  session: service('session'),
+
+  actions: {
+    authenticateWithOAuth2() {
+      let { identification, password } = this.getProperties('identification', 'password');
+      this.get('session').authenticate('authenticator:oauth2', identification, password)
+        .then(() => {
+          this.get('rememberMe') && this.set('session.store.cookieExpirationTime', 60 * 60 * 24 * 14);
+        })
+        .catch((reason) => {
+          this.set('errorMessage', reason.error);
+        });
+    }
+  }
+});
--- app/routes/login.js
+++ app/routes/login.js
@@ -1,23 +1,4 @@
-import { inject as service } from '@ember/service';
 import Route from '@ember/routing/route';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Route.extend( {
-  
-  session: service('session'),
-  email: "",
-  password: "",
-
-  actions: {
-    authenticateWithOAuth2() {
-      let { email, password } = this.getProperties('email', 'password');
-      this.get('session').authenticate('authenticator:oauth2', email, password)
-        .then(() => {
-          this.get('rememberMe') && this.set('session.store.cookieExpirationTime', 60 * 60 * 24 * 14);
-        })
-        .catch((reason) => {
-          debugger;
-          this.set('errorMessage', reason.error);
-        });
-    }
-  }
-});
+export default Route.extend(UnauthenticatedRouteMixin);
